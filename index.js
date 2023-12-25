f4dadd3375e7df8f211e6ba77b02bb2559dbe162
Pablo <pablomurbaez@yahoo.com>
2023-12-25 01:15:18 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+var num3 = 25;
+var num4 = 7;
+
+var mod = num3 % num4;
+
+console.log(mod);
+const numbers = [10, 5, 20,15];
+const max = Math.max(...numbers);
+console/log(max);
